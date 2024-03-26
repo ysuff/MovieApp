2466cab021a4617e467175f82e0651812096b5ad
yufus <yusufandic@icloud.com>
2024-03-27 02:50:08 +0300

    flm

--- app/src/main/java/com/yusuf/movieapp/FavoriteMoviesDataClass.kt
+++
@@ -1,9 +0,0 @@
-package com.yusuf.movieapp
-
-import com.google.gson.annotations.SerializedName
-
-data class FavoriteMoviesDataClass(
-    @SerializedName("id") var id: String,
-    @SerializedName("original_title") var originalTitle: String,
-    @SerializedName("popularity") var popularity: String
-)
